429303b961664c8300c4ef72826e03e2d15e98b5
luisaviveros <luisaviveros73@gmail.com>
2024-09-26 14:44:56 -0500

    boton

--- src/components/6.add-post-button/add-post-button.ts
+++ src/components/6.add-post-button/add-post-button.ts
@@ -13,11 +13,14 @@ export class addPostButton extends HTMLElement {
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-      <style>
-      ${styles}
-  </style>
- <button id="addPostBtn">Add Post</button>
-      `;
+			  <style>
+				${styles}
+			  </style>
+			  <div id="button-container"> <!-- Contenedor principal -->
+				<div class="raya"></div> <!-- La raya -->
+				<button id="addPostBtn">Add Post</button> <!-- El botón -->
+			  </div>
+			`;
 		}
 	}
 }
