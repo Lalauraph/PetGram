772807f48d354cefd07519994d2c79d074fb6a6f
Lalauraph <laurapalacio900@gmail.com>
2024-09-27 16:07:11 -0500

    feat: data updated and logo added to banner

--- src/data/dataPostCard.ts
+++ src/data/dataPostCard.ts
@@ -4,7 +4,6 @@ interface PostCardShape {
 	name: string;
 	breed: string;
 	caption: string;
-	likes: number;
 }
 
 export const PostCard: PostCardShape[] = [
@@ -14,7 +13,6 @@ export const PostCard: PostCardShape[] = [
 		name: 'Luna',
 		breed: 'Golden Retriever',
 		caption: 'loving my new bows',
-		likes: 127,
 	},
 	{
 		profilePicture: 'https://i.pinimg.com/564x/e0/b5/b6/e0b5b65a08f43e9d65ccf4b61534a717.jpg',
@@ -22,7 +20,6 @@ export const PostCard: PostCardShape[] = [
 		name: 'Prince',
 		breed: 'Orange Cat',
 		caption: 'Im getting bigger every day',
-		likes: 127,
 	},
 	{
 		profilePicture: 'https://i.pinimg.com/564x/b6/29/54/b62954f9b21cf96f3a7657f109593dd9.jpg',
@@ -30,7 +27,6 @@ export const PostCard: PostCardShape[] = [
 		name: 'Sebs',
 		breed: 'Cockatiels',
 		caption: 'I look nice from the back',
-		likes: 127,
 	},
 	{
 		profilePicture: 'https://i.pinimg.com/564x/31/1a/97/311a9752558f328ebcfc14dd3467e5e2.jpg',
@@ -38,6 +34,19 @@ export const PostCard: PostCardShape[] = [
 		name: 'Sandy',
 		breed: 'Squirrel',
 		caption: 'Conecting with nature',
-		likes: 127,
+	},
+	{
+		profilePicture: 'https://i.pinimg.com/564x/48/ac/d7/48acd7dd10e8ee4f4735bc136eb80119.jpg',
+		postPicture: 'https://i.pinimg.com/564x/e4/3f/4d/e43f4d5023d5d1be98ed156b01eb984f.jpg',
+		name: 'Bambi',
+		breed: 'Hamster',
+		caption: 'Loving my new house',
+	},
+	{
+		profilePicture: 'https://i.pinimg.com/564x/aa/0d/2d/aa0d2d89bc94573e7022caeac64301d2.jpg',
+		postPicture: 'https://i.pinimg.com/564x/2a/45/4a/2a454a20a1d2de7ee582f8698d077249.jpg',
+		name: 'Winnie',
+		breed: 'Chihuahua',
+		caption: 'Looking extra tiny today',
 	},
 ];
