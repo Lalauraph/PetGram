4d9e9af3315329310fdbff6e2c1ebd53c610ff0a
luisaviveros <luisaviveros73@gmail.com>
2024-09-26 21:36:30 -0500

    CssGeneral

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -46,10 +46,8 @@ class AppContainer extends HTMLElement {
 	connectedCallback() {
 		this.renderizar();
 	}
-
 	renderizar() {
 		if (this.shadowRoot) {
-			// this.shadowRoot.innerHTML = `<h1>TEST</h1>`;
 			this.profileBanner.forEach((profileBanner) => {
 				this.shadowRoot?.appendChild(profileBanner);
 			});
@@ -57,11 +55,31 @@ class AppContainer extends HTMLElement {
 				this.shadowRoot?.appendChild(welcomeMessage);
 			});
 			this.shadowRoot?.appendChild(this.addPostButton);
+	
+			// Crear un div para envolver las tarjetas
+			const columnDiv = document.createElement('div');
+			columnDiv.style.display = 'grid';  // Usamos grid para el diseño de las tarjetas
+			columnDiv.style.gridTemplateColumns = '1fr';  // Por defecto, una columna
+			columnDiv.style.gap = '15px';  // Espacio entre las tarjetas
+			columnDiv.style.justifyContent = 'center';  // Centramos el grid
+			columnDiv.style.margin = '0 auto';  // Centramos el contenedor
+			columnDiv.style.maxWidth = '1300px';  // Ancho máximo del contenedor
+			columnDiv.style.padding = '20px';  // Espaciado interno
+	
+			
+			const mediaQuery = window.matchMedia('(min-width: 1024px)');
+			if (mediaQuery.matches) {
+				columnDiv.style.gridTemplateColumns = 'repeat(2, 1fr)';  // Cambiamos a dos columnas en pantallas grandes
+			}
+	
 			this.postCard.forEach((postCard) => {
-				this.shadowRoot?.appendChild(postCard);
+				columnDiv.appendChild(postCard);
 			});
+	
+			this.shadowRoot.appendChild(columnDiv);
 		}
 	}
-}
+	
+}	
 
 customElements.define('app-container', AppContainer);
