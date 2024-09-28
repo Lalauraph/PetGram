d5987b306976c54d8afd5e5e0b2d3e1f350643a7
Lalauraph <laurapalacio900@gmail.com>
2024-09-27 19:30:11 -0500

    comments

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -3,7 +3,7 @@ import { Profile } from './data/dataProfile';
 import './components/indexPadre';
 import { profileBanner } from './components/4.profile-banner/profile-banner';
 import { postCard } from './components/7.post-card/post-card';
-import { welcomeMessage } from './components/13.welcome-message/welcome-message';
+import { welcomeMessage } from './components/7.post-card/13.welcome-message/welcome-message';
 import { addPostButton } from './components/6.add-post-button/add-post-button';
 // Crear el App container
 class AppContainer extends HTMLElement {
@@ -15,7 +15,7 @@ class AppContainer extends HTMLElement {
 		super();
 		this.attachShadow({ mode: 'open' });
 
-		// Crear el componente profile-banner
+		// Crear los componentes
 
 		Profile.forEach((user) => {
 			const profileBanner = document.createElement('profile-banner') as profileBanner;
@@ -55,31 +55,29 @@ class AppContainer extends HTMLElement {
 				this.shadowRoot?.appendChild(welcomeMessage);
 			});
 			this.shadowRoot?.appendChild(this.addPostButton);
-	
+
 			// Crear un div para envolver las tarjetas
 			const columnDiv = document.createElement('div');
-			columnDiv.style.display = 'grid';  // Usamos grid para el diseño de las tarjetas
-			columnDiv.style.gridTemplateColumns = '1fr';  // Por defecto, una columna
-			columnDiv.style.gap = '15px';  // Espacio entre las tarjetas
-			columnDiv.style.justifyContent = 'center';  // Centramos el grid
-			columnDiv.style.margin = '0 auto';  // Centramos el contenedor
-			columnDiv.style.maxWidth = '1300px';  // Ancho máximo del contenedor
-			columnDiv.style.padding = '20px';  // Espaciado interno
-	
-			
+			columnDiv.style.display = 'grid'; // Usamos grid para el diseño de las tarjetas
+			columnDiv.style.gridTemplateColumns = '1fr'; // Por defecto, una columna
+			columnDiv.style.gap = '15px'; // Espacio entre las tarjetas
+			columnDiv.style.justifyContent = 'center'; // Centramos el grid
+			columnDiv.style.margin = '0 auto'; // Centramos el contenedor
+			columnDiv.style.maxWidth = '1300px'; // Ancho máximo del contenedor
+			columnDiv.style.padding = '20px'; // Espaciado interno
+
 			const mediaQuery = window.matchMedia('(min-width: 1024px)');
 			if (mediaQuery.matches) {
-				columnDiv.style.gridTemplateColumns = 'repeat(2, 1fr)';  // Cambiamos a dos columnas en pantallas grandes
+				columnDiv.style.gridTemplateColumns = 'repeat(2, 1fr)'; // Cambiamos a dos columnas en pantallas grandes
 			}
-	
+
 			this.postCard.forEach((postCard) => {
 				columnDiv.appendChild(postCard);
 			});
-	
+
 			this.shadowRoot.appendChild(columnDiv);
 		}
 	}
-	
-}	
+}
 
 customElements.define('app-container', AppContainer);
