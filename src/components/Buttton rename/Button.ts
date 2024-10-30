6b3dc8eacdd4aa573e9166ed3bb105777518702a
luisaviveros <luisaviveros73@gmail.com>
2024-10-30 12:12:17 -0500

    boton

--- src/components/Buttton rename/Button.ts
+++ src/components/Buttton rename/Button.ts
@@ -12,4 +12,36 @@ export class CustomButton extends HTMLElement {
 		this.attachShadow({ mode: 'open' });
 	}
 
-	
+	static get observedAttributes() {
+		const attrs: Record<AttributeButton, null> = {
+			label: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	attributeChangedCallback(propName: string, oldValue: string | undefined, newValue: string | undefined) {
+		if (propName === AttributeButton.label) {
+			this.label = newValue;
+			this.render();
+		}
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+        <style>
+          ${styles}
+        </style>
+        <button class="custom-button">
+          ${this.label}
+        </button>
+      `;
+		}
+	}
+}
+
+customElements.define('custom-button', CustomButton);
