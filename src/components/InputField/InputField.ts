78b1f7ca345303f8e11f83272c1e8df40fda7a9f
luisaviveros <luisaviveros73@gmail.com>
2024-10-31 10:37:53 -0500

    input

---
+++ src/components/InputField/InputField.ts
@@ -0,0 +1,45 @@
+import styles from './input-field.css';
+
+export enum AttributeInput {
+	'placeholder' = 'placeholder',
+}
+
+export class InputField extends HTMLElement {
+	placeholder?: string;
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<AttributeInput, null> = {
+			placeholder: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	attributeChangedCallback(propName: string, _: string | undefined, newValue: string | undefined) {
+		if (propName === AttributeInput.placeholder) {
+			this.placeholder = newValue;
+			this.render();
+		}
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+				<style>
+					${styles}
+				</style>
+				<input class="input-field" type="text" placeholder="${this.placeholder || ''}">
+			`;
+		}
+	}
+}
+
+
