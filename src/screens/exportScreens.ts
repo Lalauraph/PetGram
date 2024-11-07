b44f234707afce5cd82ebda4f891d91712a629e4
Lalauraph <laurapalacio900@gmail.com>
2024-11-06 20:24:54 -0500

    chore: landing.ts screen code donde

--- src/screens/exportScreens.ts
+++ src/screens/exportScreens.ts
@@ -1,5 +1,5 @@
 //exportar las screens por el nombre de su clase
 export { default as LoginScreen } from './login/login';
 export { default as FeedScreen } from './feed/feed';
-export { landing } from './landing/landing';
+export { Landing } from './landing/landing';
 export { default as RegisterScreen } from './register/register';
--- src/screens/landing/landing.ts
+++ src/screens/landing/landing.ts
@@ -1,80 +1,57 @@
-//importar los componentes a usar
-import '../../components/exportComponents';
+// Importar los componentes a usar
 import { introBanner } from '../../components/exportComponents';
 import { createAccountButton } from '../../components/exportComponents';
 import { LoginButton } from '../../components/exportComponents';
+import styles from './landing.css';
 
 // Crear el App container
 export class Landing extends HTMLElement {
-	introBanner: introBanner[] = [];
-	createAccountButton: createAccountButton[] = [];
-	LoginButton: LoginButton[] = [];
+	introbanner!: introBanner;
+	createaccountbutton!: createAccountButton;
+	loginbutton!: LoginButton;
+
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
 
-		// Crear los componentes
+		// Crear componente introBanner
+		this.introbanner = document.createElement('intro-banner') as introBanner;
 
-			const introBanner = document.createElement('intro-banner') as introBanner;
-			this.introBanner.push(introBanner);
+		// Crear el componente createAccountButton
+		this.createaccountbutton = document.createElement('create-account') as createAccountButton;
 
-		});
-
-		PostCard.forEach((card) => {
-			const postCard = document.createElement('post-card') as postCard;
-			postCard.profilePicture = card.profilePicture;
-			postCard.postPicture = card.postPicture;
-			postCard.name = card.name;
-			postCard.breed = card.breed;
-			postCard.caption = card.caption;
-			this.postCard.push(postCard);
-		});
-
-		Profile.forEach((user) => {
-			const welcomeMessage = document.createElement('welcome-message') as welcomeMessage;
-			welcomeMessage.name = user.name;
-			this.welcomeMessage.push(welcomeMessage);
-		});
-
-		this.addPostButton = document.createElement('add-post-button') as addPostButton;
+		// Crear el componente LoginButton
+		this.loginbutton = document.createElement('login-button') as LoginButton;
 	}
 
 	connectedCallback() {
-		this.renderizar();
+		this.render();
 	}
-	renderizar() {
-		console.log('renderizando');
-		if (this.shadowRoot) {
-			this.profileBanner.forEach((profileBanner) => {
-				this.shadowRoot?.appendChild(profileBanner);
-			});
-			this.welcomeMessage.forEach((welcomeMessage) => {
-				this.shadowRoot?.appendChild(welcomeMessage);
-			});
-			this.shadowRoot?.appendChild(this.addPostButton);
 
-			// Crear un div para envolver las tarjetas
-			const columnDiv = document.createElement('div');
-			columnDiv.style.display = 'grid'; // Usamos grid para el diseño de las tarjetas
-			columnDiv.style.gridTemplateColumns = '1fr'; // Por defecto, una columna
-			columnDiv.style.gap = '15px'; // Espacio entre las tarjetas
-			columnDiv.style.justifyContent = 'center'; // Centramos el grid
-			columnDiv.style.margin = '0 auto'; // Centramos el contenedor
-			columnDiv.style.maxWidth = '1300px'; // Ancho máximo del contenedor
-			columnDiv.style.padding = '20px'; // Espaciado interno
+	render() {
+		// agregar los componentes al DOM
+		{
+			if (this.shadowRoot) {
+				this.shadowRoot.innerHTML = `
+					<style>
+					${styles}
+					</style>
 
-			const mediaQuery = window.matchMedia('(min-width: 1024px)');
-			if (mediaQuery.matches) {
-				columnDiv.style.gridTemplateColumns = 'repeat(2, 1fr)'; // Cambiamos a dos columnas en pantallas grandes
+					<div class="landing-container">
+						<intro-banner></intro-banner>
+						<h1 class="welcomeTitle">Welcome to</h1>
+						<img id="logo" src="https://imgur.com/eYBta3X.png">
+
+						<div class="button-container">
+							<create-account></create-account>
+							<login-button></login-button>
+						</div>
+					</div>
+				`;
 			}
-
-			this.postCard.forEach((postCard) => {
-				columnDiv.appendChild(postCard);
-			});
-
-			this.shadowRoot.appendChild(columnDiv);
 		}
 	}
 }
 
-customElements.define('app-landing', landing);
+//exportar pantalla como elemento personalizado
+customElements.define('landing-screen', Landing);
