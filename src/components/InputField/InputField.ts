bb6e33e0ddb2be652613b8f63db4ffd3012ea69f
luisaviveros <luisaviveros73@gmail.com>
2024-11-08 10:50:02 -0500

    InputFieldtss

---
+++ src/components/InputField/InputField.ts
@@ -0,0 +1,45 @@
+export class InputField extends HTMLElement {
+    label: string = '';
+    type: string = 'text';
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.label = this.getAttribute('label') || '';
+        this.type = this.getAttribute('type') || 'text';
+        this.render();
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+                <style>
+                    .input-container {
+                        margin: 10px 0;
+                    }
+                    label {
+                        color: white;
+                        display: block;
+                        font-size: 0.9em;
+                        margin-bottom: 5px;
+                    }
+                    input {
+                        width: 100%;
+                        padding: 10px;
+                        border-radius: 20px;
+                        border: none;
+                    }
+                </style>
+                <div class="input-container">
+                    <label>${this.label}</label>
+                    <input type="${this.type}">
+                </div>
+            `;
+        }
+    }
+}
+
+customElements.define('input-field', InputField);
