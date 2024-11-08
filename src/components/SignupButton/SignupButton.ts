bf78a39dc537d3cb617bd6566a94f507d58b6a87
luisaviveros <luisaviveros73@gmail.com>
2024-11-08 10:57:51 -0500

    SignupButton.TS

--- src/components/SignupButton/SignupButton.ts
+++ src/components/SignupButton/SignupButton.ts
@@ -11,18 +11,7 @@ export class SignupButton extends HTMLElement {
     render() {
         if (this.shadowRoot) {
             this.shadowRoot.innerHTML = `
-                <style>
-                    button {
-                        background-color: #ffcc00;
-                        color: black;
-                        border: none;
-                        border-radius: 20px;
-                        padding: 10px 20px;
-                        font-size: 1rem;
-                        cursor: pointer;
-                        width: 100%;
-                        margin-top: 15px;
-                    }
+
                 </style>
                 <button type="button">SIGN UP</button>
             `;
